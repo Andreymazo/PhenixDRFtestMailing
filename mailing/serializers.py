bea472a4f8ad30fb9a1aab5138b388100b876f35
andrey_mazo <andreymazo@mail.ru>
2023-07-04 10:15:33 +0300

    Добавил в сериализаторе поле телефон на ендпонте mailing_create

--- mailing/serializers.py
+++ mailing/serializers.py
@@ -4,17 +4,21 @@ from mailing.models import Clientt, Mailing, Mailinglog
 
 
 class ClientSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Clientt
         fields = '__all__'
 
 
 class MailingSerializer(serializers.ModelSerializer):
-    last_attempt = serializers.ReadOnlyField()
+    # last_attempt = serializers.ReadOnlyField()
+    phone = ClientSerializer(source='clientt', many=False)
 
     class Meta:
         model = Mailing
-        fields = '__all__'
+        fields = ['message', 'tag', 'last_attempt', 'time_completion', 'phone',]#'__all__'
+
+
 class MailinglogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mailinglog
